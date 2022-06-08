210f73ee42e88b2363c30d8d92290ac50609c3c6
ega4432 <38056766+ega4432@users.noreply.github.com>
2022-06-09 00:28:07 +0900

    bug: fixed ci error

--- bin/homebrew.sh
+++ bin/homebrew.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -eu
 
-echo "Setup homebrew ..."
+cd "$(dirname "$0")"
+echo -e "Setup homebrew ...\n"
 
 if [ "$(uname)" == "Darwin" -o "$(uname)" == "Linux" ]; then
     if ! type brew &> /dev/null ; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &> /dev/null
     else
-        echo "Homebrew is already installed."
+        echo -e "Homebrew is already installed.\n"
     fi
 fi
 
--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -eu
 
-echo "Start setup ..."
-
+echo -e "Start setup ...\n"
 if ! cd $HOME/src/github.com/ega4432/dotfiles &> /dev/null ; then
     mkdir -p $HOME/src/github.com/ega4432 && cd $_
     git clone https://github.com/ega4432/dotfiles.git dotfiles
     cd dotfiles
+    echo;
 fi
 
 find ./bin -type f -name '*.sh' | xargs -p -I {} bash -c {}
