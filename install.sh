1e4ddcdd08a85e779f5363e2270e720b1e68a2b1
ega4432 <38056766+ega4432@users.noreply.github.com>
2022-06-08 21:34:10 +0900

    chore: update install script and ci yaml

--- install.sh
+++ install.sh
@@ -2,16 +2,23 @@
 
 echo "Start setup ..."
 
-if [ $(uname) = Darwin ]; then
+if ! cd $HOME/dotfiles &> /dev/null ; then
+    git clone https://github.com/ega4432/dotfiles.git $HOME/dotfiles
+    cd $HOME/dotfiles
+fi
+
+if [ "$(uname)" = "Darwin" ]; then
     if ! type brew &> /dev/null ; then
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > /dev/null
     else
         echo "Since Homebrew is already installed, skip this phase and proceed."
     fi
-    brew update
-    brew upgrade
-    brew cleanup
-    brew bundle install --file=Brewfile
+    echo "install dependecies ..."
+    # brew update
+    # brew upgrade
+    # brew cleanup
+    # brew bundle install --file=Brewfile
 else
+    echo -n "Not macOS ..."
     echo $uname
 fi
