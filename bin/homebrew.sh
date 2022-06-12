9436a52fc46fc3398790bbaeb6de32c829510c89
ega4432 <38056766+ega4432@users.noreply.github.com>
2022-06-12 11:57:51 +0900

    bug: use non-interactive mode with install homebrew

--- bin/homebrew.sh
+++ bin/homebrew.sh
@@ -5,7 +5,8 @@ echo "---> Setup homebrew..."
 
 if [ "$(uname)" == "Darwin" ] || [ "$(uname)" == "Linux" ]; then
     if ! type brew &> /dev/null ; then
-        sudo /bin/bash -c "curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
+        read -rsp "Please input your password: " PASSWORD; echo
+        echo "$PASSWORD" | NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     else
         echo "Homebrew is already installed."
     fi
