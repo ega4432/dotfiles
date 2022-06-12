b78b698043cef85e66f733ced3082dca6f969c03
ega4432 <38056766+ega4432@users.noreply.github.com>
2022-06-12 11:33:28 +0900

    refactor: shellcheck

--- install.sh
+++ install.sh
@@ -2,17 +2,17 @@
 
 echo "Start setup..."
 
-GITHUB_DIR="$HOME/src/github.com/ega4432"
+GIT_CLONE_DIR="$HOME/src/github.com/ega4432"
 
 if ! xcode-select --print-path &> /dev/null; then
     echo "---> Installing command line tools ..."
     xcode-select --install
 fi
 
-if ! cd "$GITHUB_DIR/dotfiles" &> /dev/null; then
-    mkdir -p "$GITHUB_DIR" && cd "$_"
+if ! cd "$GIT_CLONE_DIR/dotfiles" &> /dev/null; then
+    mkdir -p "$GIT_CLONE_DIR" && cd "$_"
     git clone https://github.com/ega4432/dotfiles.git
     cd dotfiles
 fi
 
-find bin/ -type f -name '*.sh' | xargs -I {} bash -c {}
+find bin/ -type f -name '*.sh' -print0 | xargs -I {} bash -c {}
