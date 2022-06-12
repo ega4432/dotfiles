f38b964a7d3c2f1784cc17bba54ceba468837f1f
ega4432 <38056766+ega4432@users.noreply.github.com>
2022-06-12 11:33:45 +0900

    bug: fixed sudo exec

--- bin/homebrew.sh
+++ bin/homebrew.sh
@@ -5,8 +5,7 @@ echo "---> Setup homebrew..."
 
 if [ "$(uname)" == "Darwin" ] || [ "$(uname)" == "Linux" ]; then
     if ! type brew &> /dev/null ; then
-        read -sp "Please input your password: " PASSWORD; echo
-        echo "$PASSWORD" | sudo -S curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+        sudo /bin/bash -c "curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
     else
         echo "Homebrew is already installed."
     fi
