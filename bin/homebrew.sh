3a18ac69378cef7f14ca5aeb43f276bca6377002
ega4432 <38056766+ega4432@users.noreply.github.com>
2022-06-12 13:29:34 +0900

    bug: fixed install homebrew by unusing `sudo`

--- bin/homebrew.sh
+++ bin/homebrew.sh
@@ -5,7 +5,7 @@ echo "---> Setup homebrew..."
 
 if [ "$(uname)" == "Darwin" ] || [ "$(uname)" == "Linux" ]; then
     if ! type brew &> /dev/null ; then
-        NONINTERACTIVE=1 sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     else
         echo "Homebrew is already installed."
     fi
