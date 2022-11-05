01b1acd68ad735ecbdb4369769248c5e8704444a
ega4432 <38056766+ega4432@users.noreply.github.com>
2022-11-05 22:34:58 +0900

    bug: git-secrets install dir

--- bin/git-secrets.sh
+++ bin/git-secrets.sh
@@ -5,6 +5,8 @@ set -ue
 cd "$(dirname "$0")"
 echo "---> Setup git-secrets ..."
 
+GIT_CLONE_DIR=~/src/github.com/ega4432
+
 if ! type git &>/dev/null ; then
     echo "\"git\" command not found."
     exit 1
@@ -12,7 +14,11 @@ fi
 
 if ! git secrets &>/dev/null ; then
     echo "---> Installing \"git-secrets\" command ..."
-    cd ~/src/github.com/ega4432
+
+    if ! cd "$GIT_CLONE_DIR" &>/dev/null; then
+        mkdir -p "$GIT_CLONE_DIR" && cd "$_"
+    fi
+
     git clone https://github.com/awslabs/git-secrets.git
     cd git-secrets
     sudo make install
@@ -24,4 +30,4 @@ echo "---> Setup git-secrets and installing hooks files ..."
 git secrets --register-aws --global
 
 # Download hooks file to `~/.git-templates/git-secrets`
-git secrets --install ~/.git-templates/git-secrets
+# git secrets --install ~/.git-templates/git-secrets
